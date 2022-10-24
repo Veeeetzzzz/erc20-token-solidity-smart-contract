9655186e17839fc8f1930580c488fe2afd84efc1
Veeeetzzzz <40268197+Veeeetzzzz@users.noreply.github.com>
2022-10-24 12:33:44 +0100

    Update mint-and-burn.sol

--- mint-and-burn.sol
+++ mint-and-burn.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
-
+//Imports - the burn variables are in ERC20Burnable.sol
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-
+//Token attributes
 contract TokenNameGoesHere is ERC20, ERC20Burnable, Ownable {
     constructor() ERC20("TokenNameGoesHere", "SymbolGoesHere") {}
-
+//Contract owner and amount to mint
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }

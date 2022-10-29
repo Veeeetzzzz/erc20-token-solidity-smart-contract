226b1e978ce82b05ad2bf434bb034527625cdfc8
Veeeetzzzz <40268197+Veeeetzzzz@users.noreply.github.com>
2022-10-29 18:30:53 +0100

    Update basic-mint.sol

--- basic-mint.sol
+++ basic-mint.sol
@@ -1,13 +1,11 @@
 // SPDX-License-Identifier: MIT
+//Define minimum compiler version
 pragma solidity ^0.8.4;
 //Imports
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-//Token Name and Symbol
-contract TokenNameGoesHere is ERC20, Ownable {
-    constructor() ERC20("TokenNameGoesHere", "SymbolGoesHere") {}
-//Define contract owner and amount to mint
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
+//Token details with initial mint supply
+contract TOKENNAME is ERC20 {
+    constructor() ERC20("TOKANENAME", "TKN") {
+        _mint(msg.sender, 10000 * 10 ** decimals());
     }
 }
